177a464eb8075abc77f52cdaa9c4ddb53c8dd6bc
Bashir Uddin <uddinbashir1999@gmail.com>
2023-03-28 18:52:42 +0500

    fix: update alchemy configs

--- configs/alchemy.config.js
+++ configs/alchemy.config.js
@@ -1,7 +1,7 @@
 import { Alchemy, Network } from "alchemy-sdk";
 
 const settings = {
-  apiKey: process.env.NEXT_APP_ALCHEMY_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY,
   network: Network.ETH_MAINNET,
 };
 
