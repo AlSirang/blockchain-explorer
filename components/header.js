13dedc4861b995c12ccea64536ad260b82ada32f
Bashir Uddin <uddinbashir1999@gmail.com>
2023-04-06 12:11:46 +0500

    styling: fix header width

--- components/header.js
+++ components/header.js
@@ -4,14 +4,14 @@ import { EtherscanIcon } from "@/icons";
 export default function Header() {
   return (
     <header className="py-2 px-3 bg-white shadow">
-      <div className="max-w-[1700px] flex justify-between items-center m-auto w-full ">
-        <div className="max-w-7xl">
+      <div className="max-w-7xl flex justify-between items-center m-auto w-full ">
+        <span>
           <NextLink href="/">
             <EtherscanIcon className="max-w-[150px] max-h-[40px]" />
           </NextLink>
-        </div>
+        </span>
 
-        <div> Home</div>
+        <div>Home</div>
       </div>
     </header>
   );
