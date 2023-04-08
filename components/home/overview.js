40f583c6ce9c67ebb1bd01c9dd919678d21b6134
Bashir Uddin <uddinbashir1999@gmail.com>
2023-04-08 13:46:56 +0500

    fix: add overview section

---
+++ components/home/overview.js
@@ -0,0 +1,26 @@
+export default function Overview() {
+  return (
+    <section className="relative -top-7 px-5 md:px-0">
+      <div className="max-w-7xl m-auto bg-white rounded-xl drop-shadow-sm border border-stone-300">
+        <div className="py-3 px-5">
+          <div className="grid md:grid-cols-12">
+            <div className="col-span-4">
+              <div>hellow</div>
+              <div>world</div>
+            </div>
+
+            <div className="col-span-4">
+              <div>hellow</div>
+              <div>world</div>
+            </div>
+
+            <div className="col-span-4">
+              <div>hellow</div>
+              <div>world</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
