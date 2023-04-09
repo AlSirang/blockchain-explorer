8459698869fadbce42a22496f46ece96811af129
Bashir Uddin <uddinbashir1999@gmail.com>
2023-04-09 13:16:32 +0500

    fix: add font-family

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&amp;display=swap"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
