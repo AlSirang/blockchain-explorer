3130b65ce1c003ba4cb5df0fd4bfc81e474e15f0
Bashir Uddin <uddinbashir1999@gmail.com>
2023-04-17 13:51:07 +0500

    fix: update slice length

--- utils/get-shorten-address.js
+++ utils/get-shorten-address.js
@@ -1,11 +1,13 @@
-export const getShortenAddress = (address) => {
+export const getShortenAddress = (address, length = 5) => {
   if (!address) return null;
 
-  return address.slice(0, 5) + "...." + address.slice(address.length - 5);
+  return (
+    address.slice(0, length) + "...." + address.slice(address.length - length)
+  );
 };
 
-export const getShortenAddressEnd = (address) => {
+export const getShortenAddressEnd = (address, length = 10) => {
   if (!address) return null;
 
-  return address.slice(0, 10) + "....";
+  return address.slice(0, length) + "....";
 };
