20f0f900744aab61cace82f3b7b7d3792bce1ca5
Bashir Uddin <uddinbashir1999@gmail.com>
2023-04-18 18:10:11 +0500

    remove blocks info

--- alchemy-core/get-finalized-and-safe-block.js
+++ alchemy-core/get-finalized-and-safe-block.js
@@ -1,19 +1,21 @@
 import { alchemy } from "@/configs/alchemy.config";
 
 export const getFinalizedAndSafeBlock = async () => {
-  const finalized = await alchemy.core.getBlock("finalized");
-  const safe = await alchemy.core.getBlock("safe");
+  // const finalized = await alchemy.core.getBlock("finalized");
+  // const safe = await alchemy.core.getBlock("safe");
 
-  return {
-    finalized: {
-      blockNumber: finalized.number,
-      timestamp: finalized.timestamp,
-      transactions: finalized.transactions,
-    },
-    safe: {
-      blockNumber: safe.number,
-      timestamp: safe.timestamp,
-      transactions: safe.transactions,
-    },
-  };
+  return {};
+
+  // return {
+  //   finalized: {
+  //     blockNumber: finalized.number,
+  //     timestamp: finalized.timestamp,
+  //     transactions: finalized.transactions,
+  //   },
+  //   safe: {
+  //     blockNumber: safe.number,
+  //     timestamp: safe.timestamp,
+  //     transactions: safe.transactions,
+  //   },
+  // };
 };
